a5becea3b0d387a4b436bb5d648e20cb6ff0cfc1
Ebakocornelius <ebakocornelius@gmail.com>
2026-01-16 15:47:12 +0100

    working on blog creation

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -39,7 +39,46 @@
 //   async getBlogsByTenant(tenantId: string) {
 //     return this.blogModel.find({ tenantId });
 //   }
+// // }
+// import { Injectable, BadRequestException } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import slugify from 'slugify';
+
+// import { Blog } from './blog.schema';
+// import { CreateBlogDto } from './dto/create-blog.dto';
+
+// @Injectable()
+// export class BlogsService {
+//   constructor(
+//     @InjectModel(Blog.name)
+//     private readonly blogModel: Model<Blog>,
+//   ) {}
+
+//   async createBlog(dto: CreateBlogDto, tenantId: string) {
+//     const slug = slugify(dto.title, { lower: true, strict: true });
+
+//     const existingBlog = await this.blogModel.findOne({ tenantId });
+//     if (existingBlog) {
+//       throw new BadRequestException('Tenant already has a blog');
+//     }
+
+//     const blog = new this.blogModel({
+//       title: dto.title,
+//       slug,
+//       description: dto.description,
+//       tenantId,
+//     });
+
+//     return blog.save();
+//   }
+
+//   async getBlogsByTenant(tenantId: string) {
+//     return this.blogModel.find({ tenantId });
+//   }
 // }
+
+
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -73,7 +112,8 @@ export class BlogsService {
     return blog.save();
   }
 
-  async getBlogsByTenant(tenantId: string) {
-    return this.blogModel.find({ tenantId });
+  // ✅ single blog per tenant
+  async getBlogByTenant(tenantId: string): Promise<Blog | null> {
+    return this.blogModel.findOne({ tenantId }).exec();
   }
 }
