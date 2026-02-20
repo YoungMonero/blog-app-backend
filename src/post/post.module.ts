94d267d4e82bf1287ee0b5ca517fa955ce20a879
79Rcf <197534687+79Rcf@users.noreply.github.com>
2026-02-20 05:26:34 +0100

    checked

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostController } from './post.controller';
 import { PublicPostController } from './public-post.controller';
+import { CommentsController } from '../comments/comments.controller'; 
 import { PostService } from './post.service';
+import { CommentsService } from '../comments/comments.service'; 
 import { Post, PostSchema } from './post.schema';
+import { Comment, CommentSchema } from '../comments/comment.schema'; 
 import { UsersModule } from '../users/users.module'; 
 import { AuthModule } from '../auth/auth.module'; 
 import { TenantModule } from '../tenants/tenant.module';
@@ -14,7 +17,10 @@ import { NotificationModule } from '../notifications/notification.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([
+      { name: Post.name, schema: PostSchema },
+      { name: Comment.name, schema: CommentSchema } 
+    ]),
     UsersModule,
     AuthModule,
     TenantModule,
@@ -22,8 +28,16 @@ import { NotificationModule } from '../notifications/notification.module';
     BlogsModule,
     NotificationModule,
   ],
-  controllers: [PostController, PublicPostController],
-  providers: [PostService, PostStatsService],
+  controllers: [
+    PostController, 
+    PublicPostController,
+    CommentsController 
+  ],
+  providers: [
+    PostService, 
+    PostStatsService,
+    CommentsService 
+  ],
   exports: [PostService],
 })
 export class PostModule {}
